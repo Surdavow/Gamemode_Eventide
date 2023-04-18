9c5f535729554a71412edee8c104ef79d5ff91d0
Surdavow <luigisd12@outlook.com>
2023-04-17 19:14:16 -0700

    Test

--- modules/players/player_shire.cs
+++ modules/players/player_shire.cs
@@ -33,7 +33,7 @@ function PlayerShire::onTrigger(%this, %obj, %trig, %press)
 	{
 		case 0: if(%press) Eventide_Melee(%this,%obj,3.5);
 
-		case 4: if(%obj.getEnergyLevel() == %this.maxEnergy)
+		case 4: //if(%obj.getEnergyLevel() == %this.maxEnergy)
 				if(%press)
 				{
 					%obj.mountImage(GlowFaceImage, 1);					
