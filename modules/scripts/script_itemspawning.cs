4cfdeca215e84e40e3a9f0b1069097a89e567d78
Surdavow <luigisd12@outlook.com>
2023-05-16 12:58:18 -0700

    Update script_itemspawning.cs

--- modules/scripts/script_itemspawning.cs
+++ modules/scripts/script_itemspawning.cs
@@ -3,37 +3,73 @@ package Eventide_ItemSpawning
     function MiniGameSO::Reset(%minigame,%client)
 	{		
 		Parent::Reset(%minigame,%client);
-
-		%currTime = getSimTime();
-		if(%obj.lastResetTime + 5000 > %currTime) return;
-		%minigame.lastResetTime = %currTime;	
-		%minigame.randomizeEventideItems();
+		%minigame.randomizeEventideItems(true);
 	}
+
+    function MiniGameSO::Endgame(%minigame,%client)
+	{		
+		Parent::Endgame(%minigame,%client);
+		%minigame.randomizeEventideItems(false);
+	}    
 };
+if(isPackage(Eventide_ItemSpawning)) deactivatePackage(Eventide_ItemSpawning);
 activatePackage(Eventide_ItemSpawning);
 
-function MiniGameSO::randomizeEventideItems(%minigame)
-{
-	if(isObject(EventideItemSpawnSet) && EventideItemSpawnSet.getCount())
-	for(%g = 0; %g < EventideItemSpawnSet.getCount(); %g++) 
-	if(isObject(%brick = EventideItemSpawnSet.getObject(%g)))
-    {
-        %brick.setEmitter("SparkleGroundEmitter");
+function MiniGameSO::randomizeEventideItems(%minigame,%randomize)
+{	    
+    if(!isObject(EventideItemSpawnSet) || !EventideItemSpawnSet.getCount()) return;
+    
+    %randomritual[%rrl++] = "candleItem";
+    %randomritual[%rrl++] = "candleItem";
+    %randomritual[%rrl++] = "candleItem";
+    %randomritual[%rrl++] = "candleItem";
+    %randomritual[%rrl++] = "bookItem";
+    %randomritual[%rrl++] = "daggerItem";
         
-        if(strstr(strlwr(%brick.getname()),"flaregun") != -1) %brick.setItem("FlareGunItem");
-        if(strstr(strlwr(%brick.getname()),"candle") != -1) %brick.setItem("candleItem");		
-        if(strstr(strlwr(%brick.getname()),"book") != -1) %brick.setItem("bookItem");
-        if(strstr(strlwr(%brick.getname()),"gem") != -1) %brick.setItem("gem" @ getRandom(1,4) @ "Item");		    
-        if(strstr(strlwr(%brick.getname()),"radio") != -1) %brick.setItem("CRadioItem");
-        if(strstr(strlwr(%brick.getname()),"rope") != -1) %brick.setItem("Rope");
-        if(strstr(strlwr(%brick.getname()),"soda") != -1) %brick.setItem("SodaItem");
-        if(strstr(strlwr(%brick.getname()),"bar stool") != -1) %brick.setItem("sm_barStoolItem");
-        if(strstr(strlwr(%brick.getname()),"bottle") != -1) %brick.setItem("sm_bottleItem");
-        if(strstr(strlwr(%brick.getname()),"chair") != -1) %brick.setItem("sm_chairItem");
-        if(strstr(strlwr(%brick.getname()),"dagger") != -1) %brick.setItem("daggerItem");
-        if(strstr(strlwr(%brick.getname()),"folding chair") != -1) %brick.setItem("sm_foldingChairItem");
-        if(strstr(strlwr(%brick.getname()),"poolcue") != -1) %brick.setItem("sm_poolCueItem");
-        if(strstr(strlwr(%brick.getname()),"bear trap") != -1) %brick.setItem("bearTrapItem");
+    for(%g = 0; %g < EventideItemSpawnSet.getCount(); %g++) if(isObject(%brick = EventideItemSpawnSet.getObject(%g)))
+    {
+        if(%randomize)
+        {
+            switch$(strreplace(strlwr(%brick.getname()),"_",""))
+            {
+                case "ritual":  if(%rrl)
+                                {
+                                    %brick.setEmitter("SparkleGroundEmitter");
+                                    %randomnumber = getRandom(1,%rrl);
+                                    %randomritual = %randomritual[%randomnumber];
+                                    %brick.setItem(%randomritual);
+
+                                    %randomritual[%randomnumber] = %randomritual[%rrl];
+                                    %rrl--;
+                                }                      
+
+                case "item":   %brick.setEmitter("SparkleGroundEmitter");
+                                switch(getRandom(1,3))
+                                {
+                                    case 1: %brick.setItem("CRadioItem");
+                                    case 2: %brick.setItem("Rope");
+                                    case 3: %brick.setItem("SodaItem");		    
+                                }
+
+                case "weapon": %brick.setEmitter("SparkleGroundEmitter");
+                                switch(getRandom(1,8))
+                                {
+                                    case 1: %brick.setItem("sm_barStoolItem");
+                                    case 2: %brick.setItem("sm_bottleItem");
+                                    case 3: %brick.setItem("sm_chairItem");
+                                    case 4: %brick.setItem("sm_foldingChairItem");
+                                    case 5: %brick.setItem("sm_poolCueItem");
+                                    case 6: %brick.setItem("bearTrapItem");
+                                    case 7: %brick.setItem("sm_chairItem");
+                                    case 8: %brick.setItem("FlareGunItem");
+                                }
+            }
+        }
+        else 
+        {
+            %brick.setItem("none");
+            %brick.setEmitter("none");
+        }
     }
 }
 
