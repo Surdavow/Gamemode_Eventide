00cad83814766fcac281c598d7cb58fac4c0b1fa
Surdavow <luigisd12@outlook.com>
2023-05-19 02:13:44 -0700

    Update script_shopsystem.cs

--- modules/scripts/script_shopsystem.cs
+++ modules/scripts/script_shopsystem.cs
@@ -323,11 +323,23 @@ new ScriptObject(EventideSetTitleBitmapMenu)
     menuFunction[0] = "EventideSetCustomBitmap";
     menuOption[1] = "Gun";
     menuFunction[1] = "EventideSetCustomBitmap";
-    menuOption[2] = "Exit";
-    menuFunction[2] = "exitCenterprintMenu";
+    menuOption[2] = "Skull";
+    menuFunction[2] = "EventideSetCustomBitmap";
+    menuOption[3] = "Blue Ribbon";
+    menuFunction[3] = "EventideSetCustomBitmap";
+    menuOption[4] = "Car";
+    menuFunction[4] = "EventideSetCustomBitmap";
+    menuOption[5] = "Bomb";
+    menuFunction[5] = "EventideSetCustomBitmap";
+    menuOption[6] = "Trophy";
+    menuFunction[6] = "EventideSetCustomBitmap";
+    menuOption[7] = "Star";
+    menuFunction[7] = "EventideSetCustomBitmap";
+    menuOption[8] = "Exit";
+    menuFunction[8] = "exitCenterprintMenu";
 
     justify = "<just:right>";
-    menuOptionCount = 3;
+    menuOptionCount = 9;
 };
 
 function EventideSetCustomBitmap(%client,%menu,%option)
@@ -336,5 +348,11 @@ function EventideSetCustomBitmap(%client,%menu,%option)
     {
         case 0: %client.customtitlebitmap = "<bitmap:add-ons/weapon_bow/CI_arrow.png>";
         case 1: %client.customtitlebitmap = "<bitmap:add-ons/weapon_gun/CI_gun.png>";
+        case 2: %client.customtitlebitmap = "<bitmap:base/client/ui/CI/skull.png>";
+        case 3: %client.customtitlebitmap = "<bitmap:base/client/ui/CI/blueRibbon.png>";
+        case 4: %client.customtitlebitmap = "<bitmap:base/client/ui/CI/car.png>";
+        case 5: %client.customtitlebitmap = "<bitmap:base/client/ui/CI/bomb.png>";
+        case 6: %client.customtitlebitmap = "<bitmap:base/client/ui/CI/trophy.png>";
+        case 7: %client.customtitlebitmap = "<bitmap:base/client/ui/CI/star.png>";
     }
 }
