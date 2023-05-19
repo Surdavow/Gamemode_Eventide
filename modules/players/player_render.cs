06889b24c16293dd2c36db84c3f5dd8fabb74423
Surdavow <luigisd12@outlook.com>
2023-05-19 01:15:09 -0700

    Update player_render.cs

--- modules/players/player_render.cs
+++ modules/players/player_render.cs
@@ -201,7 +201,7 @@ function PlayerRender::Prepperizer(%this,%obj)
 			
 			if(!isObject(%obscure) && %dot > 0.5 && minigameCanDamage(%obj,%player) == 1 && !%player.getDataBlock().isDowned)
 			{				
-				%closeness = (VectorDist(%obj.getPosition(),%player.getPosition())*0.1);
+				%closeness = VectorDist(%obj.getPosition(),%player.getPosition())*0.375;
 				%player.damage(%obj,%player.getWorldBoxCenter(), mClampF(%closeness,1,20), $DamageType::Default);
 				%player.markedforRenderDeath = true;
 				%client.play2d("render_blind_sound");
