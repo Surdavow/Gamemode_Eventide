c88f9ece1a91431cdff38b55734bdb01af9bff9f
Luis <104567400+Surdavow@users.noreply.github.com>
2023-12-16 03:51:37 -0800

    Return default hats

--- modules/misc/player_eventide.cs
+++ modules/misc/player_eventide.cs
@@ -234,21 +234,20 @@ function EventidePlayer::EventideAppearance(%this,%obj,%client)
 		%obj.setNodeColor($secondPack[%funcclient.secondPack],%funcclient.secondPackColor);
 	}
 	
-//	if(isObject(%funcclient.customhat)) %obj.mountImage(%funcclient.customhat,2,1,addTaggedString(getColorName(%funcclient.hatColor)));
-//	else switch$(%client.hat)
-//	{
-//		case 1: if(%client.accent)
-//				{
-//					%obj.mountImage("helmetimage",2,1,addTaggedString(getColorName(%funcclient.hatColor)));	
-//					%obj.currentHat = "helmet";
-//				}		
-//				else
-//				{
-//					%obj.mountImage("hoodieimage",2,1,addTaggedString(getColorName(%funcclient.hatColor)));	
-//					%obj.currentHat = "hoodie";
-//				}
-//		default: %obj.mountImage($hat[%funcclient.hat] @ "image",2,1,addTaggedString(getColorName(%funcclient.hatColor)));
-//	}	
+	switch$(%client.hat)
+	{
+		case 1: if(%client.accent)
+				{
+					%obj.mountImage("helmetimage",2,1,addTaggedString(getColorName(%funcclient.hatColor)));	
+					%obj.currentHat = "helmet";
+				}		
+				else
+				{
+					%obj.mountImage("hoodieimage",2,1,addTaggedString(getColorName(%funcclient.hatColor)));	
+					%obj.currentHat = "hoodie";
+				}
+		default: %obj.mountImage($hat[%funcclient.hat] @ "image",2,1,addTaggedString(getColorName(%funcclient.hatColor)));
+	}	
 	
 	if(%funcclient.hip)
 	{
