405625d2f7d19fb4c6c48cc0892775d82ee39508
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-22 19:30:27 -0800

    New stuff and fix render

--- modules/scripts/script_killers.cs
+++ modules/scripts/script_killers.cs
@@ -161,7 +161,7 @@ function Player::onKillerLoop(%obj)
 		%killerclient = %obj.client;
 		%victimclient = %scan.client;
 
-		if(%this.getName() $= "PlayerRender" && VectorDist(%scan.getPosition(), %obj.getPosition()) < 15) %this.onNearPlayer(%scan);
+		if(%this.getName() $= "PlayerRender" && VectorDist(%scan.getPosition(), %obj.getPosition()) < 15) %this.onNearPlayer(%obj,%scan);
 
 		// Chase behavior
 		if (%dot > 0.45 && !isObject(containerRayCast(%obj.getEyePoint(), %scan.getMuzzlePoint(2), $TypeMasks::FxBrickObjectType | $TypeMasks::VehicleObjectType, %obj)) && !%obj.isInvisible)
