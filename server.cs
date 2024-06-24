65f9b3bbd1441b42fde2ca1e3ca4d41e911c2db4
Luis <104567400+Surdavow@users.noreply.github.com>
2024-06-23 23:59:29 -0700

    Remove old requirements

--- server.cs
+++ server.cs
@@ -4,9 +4,6 @@ if(ForceRequiredAddOn("Brick_Halloween") == $Error::AddOn_NotFound) return error
 if ($Slayer::Server::GameModeArg $= "") $isSlayerEnabled = false;
 else $isSlayerEnabled = true;
 
-//if(ForceRequiredAddOn("Server_VehicleGore") == $Error::AddOn_NotFound) return error("Server_VehicleGore is required for Gamemode_Eventide to work");
-//if(ForceRequiredAddOn("System_Instruments") == $Error::AddOn_NotFound) return error("System_Instruments is required for Gamemode_Eventide to work");
-
 exec("./prefs.cs");
 exec("./modules/scripts/module_scripts.cs");
 exec("./modules/misc/module_misc.cs");
