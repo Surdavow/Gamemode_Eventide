22ea0199133fe71f56cc373094b57960ab225f1f
Luis <104567400+Surdavow@users.noreply.github.com>
2024-06-24 23:17:12 -0700

    Update script_commands.cs

--- modules/scripts/script_commands.cs
+++ modules/scripts/script_commands.cs
@@ -17,3 +17,8 @@ function serverCmdResetMinigame(%client)
 	if(!%client.isAdmin && !isObject(%client.minigame)) return;
 	%client.minigame.reset();	
 }
+
+function serverCmdResetMG(%client)
+{
+	serverCmdResetMinigame(%client);
+}
