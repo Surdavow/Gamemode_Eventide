c0d0a261e6b959582fd6bdb851856ae48fd0d398
Luis <104567400+Surdavow@users.noreply.github.com>
2024-06-26 11:51:51 -0700

    Comments fix

--- modules/misc/module_misc.cs
+++ modules/misc/module_misc.cs
@@ -1,6 +1,3 @@
-Here is the list that executes each of the files using `exec("./filenamehere.cs");` based on the provided image:
-
-```csharp
 exec("./brick_console.cs");
 exec("./brick_ritual.cs");
 exec("./datablocks_lights.cs");
@@ -13,6 +10,3 @@ exec("./particle_killer.cs");
 exec("./particle_portal.cs");
 exec("./projectile_lightning.cs");
 exec("./datablocks_shire.cs");
-```
-
-This list covers all the files visible in the second provided image.
