3d2fc9ca5ae8bbabf56f637db0e29050538b4de4
shirecooks <121327043+shirecooks@users.noreply.github.com>
2024-08-15 19:02:41 -0400

    Downed Icon Fixes

--- modules/players/player_empty.cs
+++ modules/players/player_empty.cs
@@ -4,16 +4,16 @@ datablock fxLightData(blankBillboard)
 	flareOn = true;
 	ConstantSizeOn = true;
 	flarebitmap = "base/data/shapes/blank.png";
-	ConstantSize = 10;    
-	nearSize = 1;
-	farSize = 10;
+	ConstantSize = 2;    
+	nearSize = 2;
+	farSize = 2;
 	farDistance = 9999;
 	LinkFlare = false;
 	AnimOffsets = false;
     FadeTime = 99999;
 	
 	blendMode = 1;
-	flareColor = "1 1 1 0.5";	
+	flareColor = "1 1 1 1";	
 };
 
 datablock fxLightData(downedBillboard : blankBillboard)
