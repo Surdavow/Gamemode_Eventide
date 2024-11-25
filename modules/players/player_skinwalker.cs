abb40026191b178ec98363d2438361cd87ef97cc
Luis <104567400+Surdavow@users.noreply.github.com>
2024-11-25 00:19:31 -0800

    Update player_skinwalker.cs

--- modules/players/player_skinwalker.cs
+++ modules/players/player_skinwalker.cs
@@ -130,24 +130,16 @@ function PlayerSkinwalker::EventideAppearance(%this,%obj,%client)
 
 function PlayerSkinwalker::onTrigger(%this, %obj, %trig, %press) 
 {		
-	if(%press)
+	if(%press) switch(%trig)		
 	{
-		switch(%trig)
-		{
-			case 0: 
-				if(%obj.getEnergyLevel() <= 25) 
-				{
-					return; 
-				}
-				%obj.KillerMelee(%this, 4);				
-			case 4: 
-				if(%obj.getEnergyLevel() >= %this.maxEnergy && !isObject(%obj.victim) && !isEventPending(%obj.monstertransformschedule))
-				{
-					%this.monstertransform(%obj, false);
-				}
-		}
+		case 0: if(%obj.getEnergyLevel() <= 25) return;
+				%obj.KillerMelee(%this, 4);
+
+		case 4: if(%obj.getEnergyLevel() >= %this.maxEnergy && !isObject(%obj.victim) && !isEventPending(%obj.monstertransformschedule))				
+				%this.monstertransform(%obj, false);			
 	}
-	Parent::onTrigger(%this, %obj, %trig, %press);	
+
+	Parent::onTrigger(%this, %obj, %trig, %press);
 }
 
 function PlayerSkinwalker::onPeggFootstep(%this,%obj)
