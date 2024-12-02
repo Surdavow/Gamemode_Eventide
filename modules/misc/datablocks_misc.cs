d2a616bded778c8603e256bb147c9d67ec71da85
Luis <104567400+Surdavow@users.noreply.github.com>
2024-12-01 21:36:34 -0800

    Include length with alxGetWaveLen

--- modules/misc/datablocks_misc.cs
+++ modules/misc/datablocks_misc.cs
@@ -24,17 +24,9 @@ while(%file !$= "")
 {
     %soundName = strreplace(filename(strlwr(%file)), ".wav", "");
 
-	if(strstr(%file,"normal") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioClose3d; filename = \"" @ %file @ "\"; };");	
-	if(strstr(%file,"quiet") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioClosest3d; filename = \"" @ %file @ "\"; };");	
-	if(strstr(%file,"loud") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioDefault3d; filename = \"" @ %file @ "\"; };");
-
-	//footsteps
-	if(strstr(%file,"sounds/footsteps/") != -1)
-	{
-		if(strstr(%file,"walk") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioFSWalk; filename = \"" @ %file @ "\"; };");
-		else if(strstr(%file,"swim") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioFSWalk; filename = \"" @ %file @ "\"; };");
-		else if(strstr(%file,"run") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioFSRun; filename = \"" @ %file @ "\"; };");		
-	}
+	if(strstr(%file,"normal") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioClose3d; filename = \"" @ %file @ "\"; length =" SPC alxGetWaveLen(%file) @ "; };");
+	if(strstr(%file,"quiet") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioClosest3d; filename = \"" @ %file @ "\"; length =" SPC alxGetWaveLen(%file) @ "; };");	
+	if(strstr(%file,"loud") != -1) eval("datablock AudioProfile(" @ %soundName @ "_sound) { preload = true; description = AudioDefault3d; filename = \"" @ %file @ "\"; length =" SPC alxGetWaveLen(%file) @ "; };");
 
 	%file = findNextFile(%pattern);
 }
