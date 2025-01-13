ea66c383ab0f4195c505c0d7ac93560c1c6f4259
PrivateCaller <proforpenicillin@borrowedtime.online>
2025-01-13 16:06:27 -0600

    Improved load error handling.

--- server.cs
+++ server.cs
@@ -1,14 +1,83 @@
-// List of required add-ons
+$Eventide_loadErrors = new ScriptGroup(Eventide_loadErrors);
+%fatalError = false;
+
+//List of required add-ons
 %requiredAddOns = "Support_CustomCDN Gamemode_Slayer Event_BrickText Item_Medical Brick_Halloween Server_EnvironmentZones Weapon_Rocket_Launcher Projectile_GravityRocket Weapon_Gun";
 
-// Iterate through the array and check each add-on
-for (%i = 0; %i < getWordCount(%requiredAddOns); %i++) 
+function Eventide_storeError(%message)
 {
-    %addOn = getWord(%requiredAddOns,%i);
-    if (ForceRequiredAddOn(%addOn) == $Error::AddOn_NotFound) 
+    %error = new ScriptObject() 
     {
-        return error(%addOn @ " is required for Gamemode_Eventide to work");
+        message = %message;
+    };
+    return %error;
+}
+
+//Iterate through the array and check each add-on.
+for(%i = 0; %i < getWordCount(%requiredAddOns); %i++) 
+{
+    %addOn = getWord(%requiredAddOns, %i);
+    if(ForceRequiredAddOn(%addOn) == $Error::AddOn_NotFound) 
+    {
+        %fatalError = true;
+        %errorMessage = %addOn @ " is required for Gamemode_Eventide to work.";
+        $Eventide_loadErrors.add(Eventide_storeError(%errorMessage));
+        error(%errorMessage);
     }
+}
+
+//Check for recommended DLLs.
+%hasSelectiveGhosting = isFunction(ShapeBase, scopeToClient);
+%hasBLPython = isFunction(py_reload_module);
+if(!%hasSelectiveGhosting && !%hasBLPython)
+{
+    %errorMessage = "You don't have Selective Ghosting OR BLPython installed. You fool. Stuff will break!";
+    $Eventide_loadErrors.add(Eventide_storeError(%errorMessage));
+    error(%errorMessage);
+}
+else if(!%hasSelectiveGhosting)
+{
+    %errorMessage = "You don't have Selective Ghosting installed. You fool. Stuff will break!";
+    $Eventide_loadErrors.add(Eventide_storeError(%errorMessage));
+    error(%errorMessage);
+}
+else if(!%hasBLPython)
+{
+    %errorMessage = "You don't have BLPython installed. You fool. Stuff will break!";
+    $Eventide_loadErrors.add(Eventide_storeError(%errorMessage));
+    error(%errorMessage);
+}
+%dummyClient.delete();
+
+//Temporary, run-once package to print error messages in the chat as the host starts the server.
+package Eventide_StartupErrorMessages
+{
+    function GameConnection::startLoad(%client)
+    {
+        parent::startLoad(%client);
+
+        %dummyClient = new ScriptObject(dummyClient) //Needed to prevent `chatMessageAll` from throwing an error.
+        {
+            isSpamming = false;
+            spamMessageCount = 0;
+        };
+        for(%i = 0; %i < $Eventide_loadErrors.getCount(); %i++)
+        {
+            %error = $Eventide_loadErrors.getObject(%i);
+            chatMessageAll(%dummyClient, "ERROR:" SPC %error.message);
+        }
+        %dummyClient.delete();
+        $Eventide_loadErrors.delete();
+
+        deactivatePackage(Eventide_StartupErrorMessages);
+    }
+};
+activatePackage(Eventide_StartupErrorMessages);
+
+//Don't run execute the gamemode scripts if a fatal error was encountered.
+if(%fatalError)
+{
+    return;
 }
 
 // Execute essential scripts and preferences first
